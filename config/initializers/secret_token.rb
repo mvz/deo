b0c82d82354e9f354d5899e0ab31dc161781261e
Matijs van Zuijlen <matijs@matijs.net>
2013-11-19 20:30:15 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Deo::Application.config.secret_key_base = '55de394a8213cb30d1c2ce07aeb29a4439b4ce5a895256d82554a6e878bcb3f806de72d7e16aee51b480319cc27e24a12ee24b35c97906a1a54c5e4aa4d9a242'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Deo::Application.config.session_store :cookie_store, key: '_deo_session'
